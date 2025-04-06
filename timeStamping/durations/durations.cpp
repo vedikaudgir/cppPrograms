bbe1e168a3a03b8ed1fde2529b5807deef76a238
Vedika Udgir <vedikaudgir@gmail.com>
2025-04-07 01:19:13 +0530

    Add files via upload

---
+++ timeStamping/durations/durations.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+int main()
+{
+    auto beforeTime = system_clock :: now();
+
+    for (int i = 0; i < 100000000; i++);
+
+    auto afterTime = system_clock :: now();
+
+    auto duration = afterTime - beforeTime;
+    cout << "\nThe time taken is:" << duration.count() << " ns ";
+
+    duration = duration_cast<milliseconds>(afterTime - beforeTime);
+    cout << "\nThe time taken is:" << duration.count() << " ms ";
+}
