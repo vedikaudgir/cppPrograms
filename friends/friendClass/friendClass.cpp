990f284e6fd169bd0d56c32d0e5ddbefb6049ae8
Vedika Udgir <vedikaudgir@gmail.com>
2025-04-21 22:28:43 +0530

    Add files via upload

---
+++ friends/friendClass/friendClass.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+private:
+    string secret = "Gojo Satoru";
+public:
+    friend class B;
+};
+
+class B
+{
+public:
+    void revealSecret(A obj)
+    {
+        cout << "\nSecret of A is: " << obj.secret;
+    }    
+};
+
+int main()
+{
+    A a;
+    B b;
+    b.revealSecret(a);
+}
