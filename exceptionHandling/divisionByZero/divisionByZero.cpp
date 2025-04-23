c30462cdfa66dacd4c6275a9eaeb3589ec550682
Vedika Udgir <vedikaudgir@gmail.com>
2025-04-23 22:40:23 +0530

    Add files via upload

---
+++ exceptionHandling/divisionByZero/divisionByZero.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "\nEnter number 1: ";
+    cin >> a;
+    cout << "\nEnter number 2: ";
+    cin >> b;
+
+    cout << "\nThe sum of numbers: " << a + b;
+    cout << "\nThe difference of numbers: " << a - b;
+    cout << "\nThe product of numbers: " << a * b;
+    try
+    {
+        if (b == 0)
+        {
+            throw "Denominator cannnot be zero!";
+        }
+        cout << "\nThe division of numbers: " << a / b;
+    }
+    catch (const char *error)
+    {
+        cout << "Error found: " << error;
+    }
+}
